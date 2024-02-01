1fcbf0e720918514fde9d535b5a22cca57f2fa6c
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-02-01 16:09:33 +0500

    react-router v6

--- 26-react-router/src/components/Courses.jsx
+++ 26-react-router/src/components/Courses.jsx
@@ -1,9 +1,20 @@
 import { Link, useLocation } from "react-router-dom";
+import queryString from 'query-string';
 import courses from "../data/courses";
+import { useState } from "react";
+
+function sortCourses(courses, key) {
+    const sortedCourses = [...courses];
+    sortedCourses.sort((a, b) => a[key] > b[key]? 1 : -1 );
+    return sortCourses;
+}
 
 const Courses = () => {
     const location = useLocation();
-    console.log(location);
+    const query = queryString.parse(location.search);
+    const [sortKey, setSortKey] = useState(query.sort);
+    console.log(sortCourses(courses, sortKey));
+    const [sortedCourses, setSortedCourses] = useState();
     return (
         <>
             <h1>Courses</h1>
