9b7d8f593283d5de04ac7f70dda76b57ffda9f9b
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-02-04 13:28:01 +0500

    react-router v7 finish

--- 26-react-router/src/components/Courses.jsx
+++ 26-react-router/src/components/Courses.jsx
@@ -1,26 +1,46 @@
-import { Link, useLocation } from "react-router-dom";
-import queryString from 'query-string';
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
 import courses from "../data/courses";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function sortCourses(courses, key) {
-    const sortedCourses = [...courses];
-    sortedCourses.sort((a, b) => a[key] > b[key]? 1 : -1 );
-    return sortCourses;
+const SORT_KEYS = ["slug", "title", "id"];
+
+function sortCourses(arr, key) {
+    const sortedCourses = [...arr];
+    if (!key || !SORT_KEYS.includes(key)) {
+        return sortedCourses;
+    }
+    sortedCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+    return sortedCourses;
 }
 
 const Courses = () => {
     const location = useLocation();
+    const navigate = useNavigate();
     const query = queryString.parse(location.search);
     const [sortKey, setSortKey] = useState(query.sort);
-    console.log(sortCourses(courses, sortKey));
-    const [sortedCourses, setSortedCourses] = useState();
+    const [sortedCourses, setSortedCourses] = useState(
+        sortCourses(courses, sortKey)
+    );
+    useEffect(() => {
+        if (!SORT_KEYS.includes(sortKey)) {
+            navigate(".");
+            setSortKey();
+            setSortedCourses([...courses]);
+        }
+    }, [sortKey, navigate]);
     return (
         <>
-            <h1>Courses</h1>
-            {courses.map((course) => (
+            <h1>{sortKey ? `Courses sorted by ${sortKey}` : "Courses"}</h1>
+            {sortedCourses.map((course) => (
                 <div key={course.id}>
-                    <Link className="courseLink" to={course.slug} course={course}>{course.title}</Link>
+                    <Link
+                        className="courseLink"
+                        to={course.slug}
+                        course={course}
+                    >
+                        {course.title}
+                    </Link>
                     <br />
                 </div>
             ))}
